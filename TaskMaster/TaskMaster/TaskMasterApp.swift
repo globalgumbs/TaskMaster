4bb715b55e875dbadb2c2af2fd772546072f7591
Albert Gumbs <73035773+agumbs1@users.noreply.github.com>
2022-02-28 23:50:17 -0500

    Add files via upload

---
+++ TaskMaster/TaskMaster/TaskMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMasterApp.swift
+//  TaskMaster
+//
+//  Created by LibUser on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
