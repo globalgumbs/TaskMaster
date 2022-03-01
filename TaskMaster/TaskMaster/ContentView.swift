50ffeab08a409dcaf67858c9a5eb4a747e41da05
Albert Gumbs <73035773+agumbs1@users.noreply.github.com>
2022-03-01 02:44:20 -0500

    Add files via upload

--- TaskMaster/TaskMaster/ContentView.swift
+++ TaskMaster/TaskMaster/ContentView.swift
@@ -9,9 +9,97 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                let mySpace = Spacer()
+                    .frame(width: 10.0)
+                let myGroup = Group {
+                    CalendarButton()
+                    mySpace
+                }
+                
+                mySpace
+                myGroup
+                myGroup
+                myGroup
+                myGroup
+                myGroup
+                myGroup
+                myGroup
+                mySpace
+                
+            }.padding()
+            
+            Spacer()
+            
+            Text("No Tasks Yet")
+            
+            Spacer()
+            
+            AddTaskButton()
+            
+        }
+        
     }
+}
+
+struct AddTaskButton: View {
+    var body: some View {
+        Button(
+            
+            action: {
+                print("yo")
+            },
+            
+            label: {
+                VStack{
+                    
+                    
+                    Image(
+                        systemName: "plus.circle.fill"
+                    )
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.black)
+                    
+                    
+                    Text("Add a task")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.semibold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.black)
+                        .padding()
+                    
+                }
+                
+            }
+            
+        )
+    }
+}
+
+
+struct CalendarButton: View{
+    var body: some View{
+        let dayVar = 2
+        Button(
+            action: {
+                print("cal")
+            },
+            
+            label: {
+                Image(systemName: "\(dayVar).circle.fill")
+                    .resizable(resizingMode: .tile)
+                    .foregroundColor(.black)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 37.5, height: 37.5)
+            }
+        )
+    }
+}
+
+
+struct DateFinder {
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
